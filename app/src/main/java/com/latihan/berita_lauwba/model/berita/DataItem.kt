2ff43b426e509c2fc73abf7159665380c0b3ccdc
AgusKhaer02 <aguskkhaer@gmail.com>
2020-02-19 15:10:08 +0700

    -Mengubah file kotlin DataItem dan Response, dan membuat layout activity_berita dan activity_berita_adapter

---
+++ app/src/main/java/com/latihan/berita_lauwba/model/berita/DataItem.kt
@@ -0,0 +1,8 @@
+package com.latihan.berita_lauwba.model.berita
+
+data class DataItem(
+	val fotoBerita: String? = null,
+	val detailBerita: String? = null,
+	val tanggalBerita: String? = null,
+	val judulBerita: String? = null
+)
---
+++ app/src/main/java/com/latihan/berita_lauwba/model/berita/Response.kt
@@ -0,0 +1,5 @@
+package com.latihan.berita_lauwba.model.berita
+
+data class Response(
+	val data: List<DataItem?>? = null
+)
